bbbd5900601df420524848e16ff0d26844b91037
Jared <jaredlgw@outlook.com>
2016-11-21 09:29:09 +1100

    Skill table method

--- src/base/Database.java
+++ src/base/Database.java
@@ -2,14 +2,11 @@ package base;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Database {
-	private static String db = "jdbc:sqlite:heretic2.db";
-	private static String skillsFold = "res/skills/";
-	private static String demonsFold = "res/demons/";
-	
-	public Connection connectDB(){
+	public Connection connectDB(String db){
 		Connection conn = null;
 		try {
 			conn = DriverManager.getConnection(db);
@@ -20,6 +17,27 @@ public class Database {
 		return conn;
 	}
 	
-	
+	public void makeSkillsTable(Connection conn){
+		String sql ="CREATE TABLE skills ("+
+				"ID 	INT PRIMARY KEY NOT NULL,"+
+				"nameJP STRING,"+
+				"nameEN STRING,"+
+				"attr 	STRING,"+
+				"cost 	INT,"+
+				"power 	STRING,"+
+				"hits 	STRING,"+
+				"kuli 	STRING,"+
+				"hit 	STRING,"+
+				"targ 	STRING,"+
+				"str 	INT,"+
+				"effect STRING)";
+		
+		try(PreparedStatement pstate = conn.prepareStatement(sql)){
+			pstate.executeUpdate();
+			System.out.println("Created skills table");
+		}catch(SQLException e){
+			System.out.println(e.getMessage());;
+		}
+	}
 	
 }
