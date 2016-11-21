5a37cb8f0ac20a13798a451d01b1ca1a4af99d68
Jared <jaredlgw@outlook.com>
2016-11-21 16:23:43 +1100

    testing select from database

--- src/test/Demontest.java
+++ src/test/Demontest.java
@@ -1,7 +1,11 @@
 package test;
 import java.io.*;
+import java.sql.*;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import base.*;
 import demon.*;
 
 public class Demontest {
@@ -17,6 +21,24 @@ public class Demontest {
 	    	System.out.println(dmn.getFusion());
 	    	System.out.println(dmn.getMutate());
 	    	System.out.println(dmn.getStats().toString());
+	    	
+	    	
+	    	
+	    	String check = dmn.getSkills().getSkill(0).getName();
+	    	System.out.println(check);
+	    	
+	    	
+	    	Database db = new Database();
+    		String sql = "SELECT nameEN FROM skills WHERE nameJP = ?";
+	    	try(Connection conn = db.connectDB("heretic.db");
+	    			PreparedStatement pstate = conn.prepareStatement(sql)){
+	    		pstate.setString(1, check);
+	    		ResultSet rset = pstate.executeQuery();
+	    		System.out.println(rset.getString("nameEN"));
+	    	}catch(Exception e ){
+	    		System.out.println(e.getMessage());
+	    	}
+	    	
 	    	System.exit(0);
 	    }catch(IOException e){
 	    	System.out.println(e.getMessage());
