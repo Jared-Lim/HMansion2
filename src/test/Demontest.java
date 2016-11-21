ef6ee61468eadd8929f948ad8f43a6ed294cad85
Jared <jaredlgw@outlook.com>
2016-11-21 12:06:37 +1100

    compacted imports

--- src/test/Demontest.java
+++ src/test/Demontest.java
@@ -1,11 +1,7 @@
 package test;
-import java.io.IOException;
-import java.io.FileReader;
-import java.io.Reader;
-
+import java.io.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import demon.*;
 
 public class Demontest {
@@ -15,12 +11,12 @@ public class Demontest {
 		Gson gson = builder.create();
 	    
 	    try(Reader reader = new FileReader("res/demons/amatsukami/amaterasu.json")){
-	    	Demon demoneeho = gson.fromJson(reader, Demon.class);
-	    	System.out.println(demoneeho.getNameEN());
-	    	System.out.println(demoneeho.getSkills().toString());
-	    	System.out.println(demoneeho.getFusion());
-	    	System.out.println(demoneeho.getMutate());
-	    	System.out.println(demoneeho.getStats().toString());
+	    	Demon dmn = gson.fromJson(reader, Demon.class);
+	    	System.out.println(dmn.getNameEN());
+	    	System.out.println(dmn.getSkills().toString());
+	    	System.out.println(dmn.getFusion());
+	    	System.out.println(dmn.getMutate());
+	    	System.out.println(dmn.getStats().toString());
 	    	System.exit(0);
 	    }catch(IOException e){
 	    	System.out.println(e.getMessage());
