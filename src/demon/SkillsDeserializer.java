094bf7f95133b57b70fd80f76bbaff655d172e18
Jared <jaredlgw@outlook.com>
2016-11-21 17:07:46 +1100

    Translation works, however multiple connections might be bad

--- src/demon/SkillsDeserializer.java
+++ src/demon/SkillsDeserializer.java
@@ -1,9 +1,21 @@
 package demon;
 
 import java.lang.reflect.Type;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-import com.google.gson.*;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import base.Database;
 
 public class SkillsDeserializer implements JsonDeserializer<Skills>{
 
@@ -16,7 +28,18 @@ public class SkillsDeserializer implements JsonDeserializer<Skills>{
 		
 		for(Map.Entry<String, JsonElement> entry : obj.entrySet()){
 			SingleSkill skl = new SingleSkill();
-			skl.setName(entry.getKey());
+			String nameJP = entry.getKey();
+    		String sql = "SELECT nameEN FROM skills WHERE nameJP = ?";
+			Database db = new Database();
+			try(Connection conn = db.connectDB("heretic.db");
+					PreparedStatement pstate = conn.prepareStatement(sql)){
+				pstate.setString(1, nameJP);
+	    		ResultSet rset = pstate.executeQuery();
+				skl.setName(rset.getString("nameEN"));
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+				skl.setName(entry.getKey());
+			}
 			skl.setLevel(entry.getValue().getAsInt());
 			SkillsList.add(skl);
 		}
--- src/test/DBtest.java
+++ src/test/DBtest.java
@@ -1,6 +1,6 @@
 package test;
 
-import java.sql.*;
+import java.sql.Connection;
 import base.*;
 
 public class DBtest {
@@ -9,7 +9,7 @@ public class DBtest {
 		try(Connection conn = db.connectDB("heretic.db")){
 			db.makeSkillsTable(conn);
 			db.parseSkillsFolder(conn);
-		}catch(SQLException e){
+		}catch(Exception e){
 			System.out.println(e.getMessage());
 		}
 	}
