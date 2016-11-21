cc7c0829cde9e2a0aef2b7546843f57978d55a64
Jared <jaredlgw@outlook.com>
2016-11-21 16:23:26 +1100

    compacted imports

--- src/demon/SkillsDeserializer.java
+++ src/demon/SkillsDeserializer.java
@@ -1,15 +1,9 @@
 package demon;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
+import com.google.gson.*;
 
 public class SkillsDeserializer implements JsonDeserializer<Skills>{
 
