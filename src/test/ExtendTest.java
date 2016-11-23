6194b530ed9e678724308574740e7b5442b0df94
Jared <jaredlgw@outlook.com>
2016-11-23 17:03:28 +0800

    testing to commence soon

--- src/test/ExtendTest.java
+++ src/test/ExtendTest.java
@@ -8,13 +8,16 @@ public class ExtendTest {
 		SkillsMaker skm = new SkillsMaker(db);
 		skm.makeSkillsTable();
 		skm.parseSkillsFolder();
+		skm.close();
 		
 		DemonTranslate dts = new DemonTranslate(db);
 		dts.makeDemonsTranslateTable();
 		dts.parseDemonsFolderTranslate();
+		dts.close();
 		
 		DemonMaker dmk = new DemonMaker(db);
 		dmk.makeDemonsTable();
 		dmk.parseDemonsFolder();
+		dmk.close();
 	}
 }
